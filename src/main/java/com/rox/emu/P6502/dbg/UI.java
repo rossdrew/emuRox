4f08f0a81db1d0aa5a29c3a0bfc6b12003154d23
rossdrew <thek1ng13@hotmail.com>
2016-12-30 13:07:19 +0000

    Adding a step button and sample program to debugger

--- src/main/java/com/rox/emu/P6502/dbg/UI.java
+++ src/main/java/com/rox/emu/P6502/dbg/UI.java
@@ -1,7 +1,17 @@
 package com.rox.emu.P6502.dbg;
 
+import com.rox.emu.Memory;
+import com.rox.emu.P6502.CPU;
+import com.rox.emu.P6502.Registers;
+import com.rox.emu.SimpleMemory;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static com.rox.emu.P6502.InstructionSet.*;
+import static com.rox.emu.P6502.InstructionSet.OP_LDA_Z_IX;
 
 /**
  * A UI for debugging 6502 CPU code
@@ -9,13 +19,49 @@ import java.awt.*;
  * @author Ross Drew
  */
 public class UI extends JFrame{
+    private CPU processor;
+    private Memory memory;
+
+    private RegistersPanel registersPanel = new RegistersPanel();
+
     public UI(){
         super("6502 Debugger");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,500);
+        setSize(800,500);
         setVisible(true);
 
-        add(new RegistersPanel());
+        setLayout(new BorderLayout());
+
+        add(registersPanel, BorderLayout.CENTER);
+
+        JButton stepButton = new JButton("Step >>");
+        stepButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                step();
+            }
+        });
+        add(stepButton, BorderLayout.SOUTH);
+
+        loadProgram(new int[] {OP_LDA_I, 99});
+    }
+
+    public void loadProgram(int[] program){
+        memory = new SimpleMemory(65534);
+        memory.setMemory(0, program);
+        processor = new CPU(memory);
+        registersPanel.setRegisters(processor.getRegisters());
+        reset();
+    }
+
+    public void reset(){
+        processor.reset();
+        registersPanel.invalidate();
+    }
+
+    public void step(){
+        processor.step();
+        registersPanel.invalidate();
     }
 
     public static void main(String[] args){
@@ -23,6 +69,8 @@ public class UI extends JFrame{
     }
 
     private class RegistersPanel extends JPanel {
+        private Registers registers;
+
         private int bitSize = 40;
         private int byteSize = (bitSize*8);
         private int padding = 4;
@@ -64,7 +112,7 @@ public class UI extends JFrame{
             g.setColor(Color.lightGray);
             for (int i=1; i<7; i++){
                 drawBit(g, startX + (i*bitSize), startY);
-                //   g.drawChars(new char[] {'N'}, 0, 1, startX + (i*bitSize) + bitSize/2, startY + bitSize/2);
+                //g.drawChars(new char[] {'N'}, 0, 1, startX + (i*bitSize) + bitSize/2, startY + bitSize/2);
             }
             g.setColor(Color.BLACK);
             g.drawRect(startX, startY, byteSize, bitSize);
@@ -72,6 +120,10 @@ public class UI extends JFrame{
 
         private void drawBit(Graphics g, int startX, int startY){
             g.drawRect(startX, startY, bitSize, bitSize);
+        }
+
+        public void setRegisters(Registers registers) {
+            this.registers = registers;
         }
     }
 }
