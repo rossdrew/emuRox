7e5d4b0500a49038188aab16057b9017339389ce
rossdrew <thek1ng13@hotmail.com>
2017-01-02 13:32:33 +0000

    EXpanded tests for BVS

--- src/test/groovy/com/rox/emu/P6502/OpCodeSpec.groovy
+++ src/test/groovy/com/rox/emu/P6502/OpCodeSpec.groovy
@@ -976,7 +976,7 @@ class OpCodeSpec extends Specification {
         0b11111110       | 0b10000100 | 5            | 0x6        | "No jump"
     }
 
-    @Unroll("BVC #expected: With accumulator set to #accumulatorValue, we end up at mem[#expectedPC] after #instructions steps")
+    @Unroll("BVC #expected: #accumulatorValue + #addedValue, checking overflow we end up at mem[#expectedPC] after #instructions steps")
     def testBVC(){
         when:
         Memory memory = new SimpleMemory(65534);
@@ -999,6 +999,31 @@ class OpCodeSpec extends Specification {
         0                | 0          | 4          | 6            | 0xD        | "Standard forward jump"
         0                | 0          | 0b10000100 | 6            | 0x5        | "Standard backward jump"
         0x50             | 0x50       | 0b10000100 | 6            | 0x8        | "No jump"
+    }
+
+    @Unroll("BVC #expected: #accumulatorValue + #addedValue, checking overflow we end up at mem[#expectedPC] after #instructions steps")
+    def testBVS(){
+        when:
+        Memory memory = new SimpleMemory(65534);
+        int[] program = [OP_NOP, OP_NOP, OP_NOP, OP_LDA_I, accumulatorValue, OP_ADC_I, addedValue, OP_BVS, jumpSteps, OP_NOP, OP_NOP, OP_NOP];
+        memory.setMemory(0, program);
+
+        and:
+        CPU processor = new CPU(memory)
+        processor.reset()
+        Registers registers = processor.getRegisters()
+
+        and:
+        processor.step(instructions)
+
+        then:
+        registers.getPC() == expectedPC
+
+        where:
+        accumulatorValue | addedValue | jumpSteps  | instructions | expectedPC | expected
+        0x50             | 0x50       | 4          | 6            | 0xD        | "Standard forward jump"
+        0x50             | 0x50       | 0b10000100 | 6            | 0x5        | "Standard backward jump"
+        0                | 0          | 0b10000100 | 6            | 0x8        | "No jump"
     }
 
 //    @Ignore
