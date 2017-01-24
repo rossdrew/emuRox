731a753181fbba396057060c9dcd17937ee0aba0
rossdrew <thek1ng13@hotmail.com>
2017-01-24 21:12:28 +0000

    Extended tests for DEC (Zero Page)

--- src/test/groovy/com/rox/emu/p6502/OpCodeSpec.groovy
+++ src/test/groovy/com/rox/emu/p6502/OpCodeSpec.groovy
@@ -764,6 +764,32 @@ class OpCodeSpec extends Specification {
         0b11111111 | 0x0         | true   | false | "Increment to zero"
     }
 
+    @Unroll("DEC (Zero Page) #Expected: on #firstValue = #expectedMem")
+    def testDEC_Z(){
+        when:
+        Memory memory = new SimpleMemory(65534);
+        int[] program = [OP_LDA_I, firstValue, OP_STA_Z, 0x20, OP_DEC_Z, 0x20]
+        memory.setMemory(0, program);
+
+        and:
+        CPU processor = new CPU(memory)
+        processor.reset()
+        processor.step(3)
+        Registers registers = processor.getRegisters()
+
+        then:
+        memory.getByte(0x20) == expectedMem
+        registers.getPC() == program.length
+        Z == registers.statusFlags[Registers.Z]
+        N == registers.statusFlags[Registers.N]
+
+        where:
+        firstValue | expectedMem | Z      | N     | Expected
+        9          | 8           | false  | false | "Simple decrement"
+        0xFF       | 0xFE        | false  | true  | "Decrement to negative value"
+        0b00000001 | 0x0         | true   | false | "Decrement to zero"
+    }
+
     @Unroll("DEX #Expected: on #firstValue = #expectedX")
     def testDEX(){
         when:
