520ff0bb8ca175e8af74f913590bfd8ac6d74095
rossdrew <thek1ng13@hotmail.com>
2017-01-28 19:03:15 +0000

    Basic implementation of SED

--- src/main/java/com/rox/emu/p6502/CPU.java
+++ src/main/java/com/rox/emu/p6502/CPU.java
@@ -478,6 +478,10 @@ public class CPU {
                 registers.clearFlag(Registers.STATUS_FLAG_IRQ_DISABLE);
                 break;
 
+            case InstructionSet.OP_SED:
+                registers.setFlag(Registers.STATUS_FLAG_DEC);
+                break;
+
             default:
                 throw new UnknownOpCodeException("Unknown 6502 OpCode:" + opCode + " encountered.", opCode);
         }
