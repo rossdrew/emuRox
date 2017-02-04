21db156302345058278c0af272fae68a9f307622
rossdrew <thek1ng13@hotmail.com>
2017-02-04 22:19:21 +0000

    Opcode for STY (Zero Page[X])

--- src/main/java/com/rox/emu/p6502/InstructionSet.java
+++ src/main/java/com/rox/emu/p6502/InstructionSet.java
@@ -51,6 +51,7 @@ public class InstructionSet {
     public static final int OP_LDX_Z = 0xA6;
     public static final int OP_LDX_Z_IY = 0xB6;
     public static final int OP_STY_Z = 0x84;
+    public static final int OP_STY_Z_IX = 0x94;
     public static final int OP_STA_Z = 0x85;
     public static final int OP_STA_ABS = 0x8D;
     public static final int OP_STA_Z_IX = 0x95;
@@ -109,7 +110,7 @@ public class InstructionSet {
                                                 OP_ADC_Z, OP_ADC_I, OP_ADC_ABS, OP_ADC_Z_IX,
                                                 OP_SBC_I, OP_SBC_Z,
                                                 OP_LDA_Z, OP_LDA_I, OP_LDA_ABS, OP_LDA_Z_IX, OP_LDA_ABS_IY, OP_LDA_ABS_IX,
-                                                OP_STA_Z, OP_STA_ABS, OP_STA_Z_IX, OP_STA_ABS_IX, OP_STX_Z, OP_STY_Z,
+                                                OP_STA_Z, OP_STA_ABS, OP_STA_Z_IX, OP_STA_ABS_IX, OP_STX_Z, OP_STY_Z, OP_STY_Z_IX,
                                                 OP_AND_I, OP_AND_Z, OP_AND_Z_IX, OP_AND_ABS_IX, OP_AND_ABS, OP_ORA_I, OP_ORA_Z, OP_EOR_I, OP_EOR_Z, OP_BIT_Z, OP_BIT_ABS,
                                                 OP_SEC, OP_CLC, OP_CLV, OP_PHA, OP_PLA,
                                                 OP_LDY_I, OP_LDY_Z, OP_LDY_Z_IX, OP_LDY_ABS, OP_INY, OP_INC_Z, OP_INC_Z_IX, OP_INC_ABS, OP_INC_ABS_IX, OP_DEC_Z, OP_DEC_ABS, OP_DEY,
@@ -171,6 +172,7 @@ public class InstructionSet {
             case OP_LDX_Z_IY:   return "LDX (Zero Page[Y])";
             case OP_LDX_ABS:    return "LDX (Absolute)";
             case OP_STY_Z:      return "STY (Zero Page)";
+            case OP_STY_Z_IX:   return "STY (Zero Page[X])";
             case OP_STA_Z:      return "STA (Zero Page)";
             case OP_STA_ABS:    return "STA (Absolute)";
             case OP_STA_Z_IX:   return "STA (Zero Page[X])";
