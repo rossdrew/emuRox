47bd808b277556f29aed39873be4a6d06ba3e916
rossdrew <thek1ng13@hotmail.com>
2017-02-15 11:05:42 +0000

    Refactor of DEC instruction to use common memory access method

--- src/main/java/com/rox/emu/p6502/CPU.java
+++ src/main/java/com/rox/emu/p6502/CPU.java
@@ -289,26 +289,24 @@ public class CPU {
 
             case InstructionSet.OP_DEC_Z: {
                 int decrementLocation = nextProgramByte();
-                int decrementedValue = (memory.getByte(decrementLocation) - 1) & 0xFF;
+                int decrementedValue = (getByteOfMemoryAt(decrementLocation) - 1) & 0xFF;
                 registers.setFlagsBasedOn(decrementedValue);
-                memory.setByteAt(decrementLocation, decrementedValue);
+                setByteOfMemoryXIndexedAt(decrementLocation, decrementedValue);
             }break;
 
             case InstructionSet.OP_DEC_Z_IX: {
                 int decrementLocation = nextProgramByte();
                 int value = getByteOfMemoryXIndexedAt(decrementLocation);
                 int decrementedValue = (value - 1) & 0xFF;
-                System.out.println("#### [" + decrementLocation +"] " + value + " := " + decrementedValue);
                 registers.setFlagsBasedOn(decrementedValue);
-
                 setByteOfMemoryXIndexedAt(decrementLocation, decrementedValue);
             }break;
 
             case InstructionSet.OP_DEC_ABS: {
                 int decrementLocation = nextProgramWord();
-                int decrementedValue = (memory.getByte(decrementLocation) - 1) & 0xFF;
+                int decrementedValue = (getByteOfMemoryAt(decrementLocation) - 1) & 0xFF;
                 registers.setFlagsBasedOn(decrementedValue);
-                memory.setByteAt(decrementLocation, decrementedValue);
+                setByteOfMemoryAt(decrementLocation, 0, decrementedValue);
             }break;
 
             case InstructionSet.OP_DEC_ABS_IX: {
