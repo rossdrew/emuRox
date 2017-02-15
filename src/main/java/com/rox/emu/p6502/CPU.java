2207fa42635195d6e3e8028b927ea58e6e33ad94
rossdrew <thek1ng13@hotmail.com>
2017-02-15 10:51:00 +0000

    Implementation of SBC (Absolute[Y])

--- src/main/java/com/rox/emu/p6502/CPU.java
+++ src/main/java/com/rox/emu/p6502/CPU.java
@@ -527,6 +527,10 @@ public class CPU {
                 registers.setRegisterAndFlags(Registers.REG_ACCUMULATOR, performSBC(getByteOfMemoryXIndexedAt(nextProgramWord())));
                 break;
 
+            case InstructionSet.OP_SBC_ABS_IY:
+                registers.setRegisterAndFlags(Registers.REG_ACCUMULATOR, performSBC(getByteOfMemoryYIndexedAt(nextProgramWord())));
+                break;
+
             case InstructionSet.OP_STY_Z:
                 memory.setByteAt(nextProgramByte(), registers.getRegister(Registers.REG_Y_INDEX));
                 break;
