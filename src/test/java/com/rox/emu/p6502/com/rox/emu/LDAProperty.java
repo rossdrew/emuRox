067d9ddcf824d023409cf6df4ae39522c04e8e67
rossdrew <thek1ng13@hotmail.com>
2017-02-18 22:32:33 +0000

    Commenting out disfunctional, experimental property based testing code

--- src/test/java/com/rox/emu/p6502/com/rox/emu/LDAProperty.java
+++ src/test/java/com/rox/emu/p6502/com/rox/emu/LDAProperty.java
@@ -12,31 +12,31 @@ import org.junit.runner.RunWith;
 import static com.rox.emu.p6502.InstructionSet.OP_LDA_I;
 import static junit.framework.TestCase.assertEquals;
 
-@RunWith(JUnitQuickcheck.class)
+//@RunWith(JUnitQuickcheck.class)
 public class LDAProperty {
     private Memory memory;
     private CPU processor;
 
-    @Before
-    public void setUp() {
-        memory = new SimpleMemory(65534);
-        memory.setByteAt(0x0, 0xFFFC);
-        memory.setByteAt(0x0, 0xFFFD);
-
-        processor = new CPU(memory);
-        processor.reset();
-    }
-
-    //No Generators are being loaded for int in the GeneratorRepository
-    @Property
-    public void loadAccumulator(int byteValue){
-        int[] program = {OP_LDA_I, byteValue};
-        memory.setMemory(0, program);
-
-        processor.step();
-
-        Registers registers = processor.getRegisters();
-        assertEquals(byteValue, registers.getRegister(Registers.REG_ACCUMULATOR));
-        assertEquals(program.length, registers.getPC());
-    }
+//    @Before
+//    public void setUp() {
+//        memory = new SimpleMemory(65534);
+//        memory.setByteAt(0x0, 0xFFFC);
+//        memory.setByteAt(0x0, 0xFFFD);
+//
+//        processor = new CPU(memory);
+//        processor.reset();
+//    }
+//
+//    //No Generators are being loaded for int in the GeneratorRepository
+//    @Property
+//    public void loadAccumulator(int byteValue){
+//        int[] program = {OP_LDA_I, byteValue};
+//        memory.setMemory(0, program);
+//
+//        processor.step();
+//
+//        Registers registers = processor.getRegisters();
+//        assertEquals(byteValue, registers.getRegister(Registers.REG_ACCUMULATOR));
+//        assertEquals(program.length, registers.getPC());
+//    }
 }
