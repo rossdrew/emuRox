1416d92cc003da1bb79fa7d9c5bb3e91b05459db
rossdrew <thek1ng13@hotmail.com>
2017-02-21 16:52:41 +0000

    Adding asserts to test

--- src/test/java/com/rox/emu/p6502/op/OpCodeNameConverterTest.java
+++ src/test/java/com/rox/emu/p6502/op/OpCodeNameConverterTest.java
@@ -2,10 +2,13 @@ package com.rox.emu.p6502.op;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
 public class OpCodeNameConverterTest {
     @Test
     public void testValidOpcode(){
         String description = OpCodeNameConverter.toDescription("OP_SEC");
-        System.out.println(description);
+        assertFalse(description == null);
+        assertFalse(description.isEmpty());
     }
 }
