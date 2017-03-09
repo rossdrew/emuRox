f05789f7121ce172071b7e4028738aa6d148401e
rossdrew <thek1ng13@hotmail.com>
2017-03-09 10:34:28 +0000

    Expanding test cases

--- src/test/groovy/com/rox/emu/p6502/op/OpCodeConverterSpec.groovy
+++ src/test/groovy/com/rox/emu/p6502/op/OpCodeConverterSpec.groovy
@@ -17,12 +17,14 @@ class OpCodeConverterSpec extends Specification{
         description == expectedDescription
 
         where:
-        opCodeName   | expectedDescription               | expected
-        'OP_SEC'     | "SEC ($OpCodeConverter.ADDR_IMP)" | "Implied Addressed"
-        'OP_ORA_I'   | "ORA ($OpCodeConverter.ADDR_I)"   | "Immediately Addressed"
-        'OP_LSR_A'   | "LSR ($OpCodeConverter.ADDR_A)"   | "Accumulator Addressed"
-        'OP_LSR_Z'   | "LSR ($OpCodeConverter.ADDR_Z)"   | "Zero Page Addressed"
-        'OP_LSR_ABS' | "LSR ($OpCodeConverter.ADDR_ABS)" | "Absolutely Addressed"
+        opCodeName      | expectedDescription               | expected
+        'OP_SEC'        | "SEC ($OpCodeConverter.ADDR_IMP)" | "Implied Addressed"
+        'OP_ORA_I'      | "ORA ($OpCodeConverter.ADDR_I)"   | "Immediately Addressed"
+        'OP_LSR_A'      | "LSR ($OpCodeConverter.ADDR_A)"   | "Accumulator Addressed"
+        'OP_LSR_Z'      | "LSR ($OpCodeConverter.ADDR_Z)"   | "Zero Page Addressed"
+        'OP_LSR_ABS'    | "LSR ($OpCodeConverter.ADDR_ABS)" | "Absolutely Addressed"
+        'OP_AND_IND_IX' | "AND ($OpCodeConverter.ADDR_IND" +
+                          "$OpCodeConverter.INDEX_X)"       | "Indirectly Addressed"
     }
 
     @Unroll("Invalid op-code: #expected")
@@ -45,8 +47,19 @@ class OpCodeConverterSpec extends Specification{
 
     /*
      * JaCoCo doesn't cover String switch statements properly, so we
-     * need a case where two Strings have the same hascode but
+     * need a case where two Strings have the same hashcode but
      * different .equals()
+     *
+     * This is because String switch statements compile to a hashcode
+     * check then an equals() check so the negative case on the
+     * equals() isn't checked by standard unit tests:-
+     *
+     * switch (s.hashCode()) {  // switch on String hashcode()
+     *   case 65:               // Case selected by hascode() value
+     *     if (s.equals("I"))   // Confirmed with equals()
+     *
+     * ref:
+     * http://stackoverflow.com/questions/42642840/why-is-jacoco-not-covering-my-switch-statements/42680333?noredirect=1#comment72503143_42680333
      */
     @Unroll("JaCoCo: Invalid (#expected) op-code")
     testAroundJaCoCoIssue(){
@@ -67,5 +80,6 @@ class OpCodeConverterSpec extends Specification{
         '\0OP_LSR_A'   | "Accumulator Addressed"
         '\0OP_LSR_Z'   | "Zero Page Addressed"
         '\0OP_LSR_ABS' | "Absolutely Addressed"
+
     }
 }
