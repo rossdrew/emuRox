af53462bf918020c8e9a0a900a769df4203277e4
rossdrew <thek1ng13@hotmail.com>
2017-04-10 16:41:34 +0100

    Adding instruction bytes number to addressing modes

--- src/main/java/com/rox/emu/p6502/op/AddressingMode.java
+++ src/main/java/com/rox/emu/p6502/op/AddressingMode.java
@@ -1,23 +1,25 @@
 package com.rox.emu.p6502.op;
 
 public enum AddressingMode {
-    IMPLIED("Implied"),
-    IMMEDIATE("Immediate"),
-    ZERO_PAGE("Zero Page"),
-    ZERO_PAGE_X("Zero Page [X]"),
-    ZERO_PAGE_Y("Zero Page [Y]"),
-    ABSOLUTE("Absolute"),
-    ABSOLUTE_X("Absolute [X]"),
-    ABSOLUTE_Y("Absolute [Y]"),
-    INDIRECT("Indirect"),
-    INDIRECT_X("Indirect, X"),
-    INDIRECT_Y("Indirect, Y"),
-    ACCUMULATOR("Accumulator");
+    IMPLIED("Implied", 1),
+    IMMEDIATE("Immediate", 2),
+    ZERO_PAGE("Zero Page", 2),
+    ZERO_PAGE_X("Zero Page [X]", 2),
+    ZERO_PAGE_Y("Zero Page [Y]", 2),
+    ABSOLUTE("Absolute", 3),
+    ABSOLUTE_X("Absolute [X]", 3),
+    ABSOLUTE_Y("Absolute [Y]", 3),
+    INDIRECT("Indirect", 2),
+    INDIRECT_X("Indirect, X", 2),
+    INDIRECT_Y("Indirect, Y", 2),
+    ACCUMULATOR("Accumulator", 1);
 
     private final String description;
+    private final int instructionBytes;
 
-    AddressingMode(String description) {
+    AddressingMode(String description, int instructionBytes) {
         this.description = description;
+        this.instructionBytes = instructionBytes;
     }
 
     public String getDescription(){
