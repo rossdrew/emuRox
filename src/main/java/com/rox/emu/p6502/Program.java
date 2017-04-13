7f1b6bb4ca10d28bca4ab6d0bdc436318ce3b6fa
rossdrew <thek1ng13@hotmail.com>
2017-04-13 15:09:49 +0100

    Adding a program builder class for neatly translating OpCodes to byte array programs

---
+++ src/main/java/com/rox/emu/p6502/Program.java
@@ -0,0 +1,48 @@
+package com.rox.emu.p6502;
+
+
+import com.rox.emu.p6502.op.OpCode;
+
+public class Program {
+    private int[] programBytes = new int[] {};
+
+    private Program(int[] programBytes){
+        this.programBytes = programBytes;
+    }
+
+    public Program with(int byteValue){
+        int[] newProgramBytes = new int[programBytes.length + 1];
+        System.arraycopy(programBytes,0, newProgramBytes, 0, programBytes.length);
+        newProgramBytes[newProgramBytes.length-1] = byteValue;
+        return new Program(newProgramBytes);
+    }
+
+    public Program with(OpCode opCode){
+        return this.with(opCode.getByteValue());
+    }
+
+    public Program with(Object value){
+        //XXX I'd need to abstract away the concept of program-byte in order to make this nicer
+        if (value instanceof OpCode)
+            return this.with((OpCode) value);
+
+        if (value instanceof Integer)
+            return this.with((int)value);
+
+        return this;
+    }
+
+    public Program with(Object ... values){
+        Program tmpProgram = this;
+
+        for (Object value : values) {
+            tmpProgram = tmpProgram.with(value);
+        }
+
+        return tmpProgram;
+    }
+
+    public int[] getProgramAsByteArray() {
+        return programBytes.clone();
+    }
+}
--- src/test/groovy/com/rox/emu/p6502/ProgramBuilder.java
+++
@@ -1,5 +0,0 @@
-package com.rox.emu.p6502;
-
-public class ProgramBuilder {
-
-}
