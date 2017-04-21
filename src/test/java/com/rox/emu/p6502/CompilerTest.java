ecbb722b3ae6284086b2e75fa43e04d5da124437
rossdrew <thek1ng13@hotmail.com>
2017-04-21 15:30:20 +0100

    Adding some compiler notes for later

--- src/test/java/com/rox/emu/p6502/CompilerTest.java
+++ src/test/java/com/rox/emu/p6502/CompilerTest.java
@@ -8,7 +8,22 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-
+/**
+ * # - Memory
+ * $ - Value
+ *
+ *  #$V               - Immediate
+ *  $V / $ VV         - Zero Page
+ *  $V,X / $VV,X      - Zero Page[X]
+ *  $V,Y / $VV,Y      - Zero Page[Y]
+ *  $VVV / $VVVV      - Absolute
+ *  $VVV,X / $VVVV,X  - Absolute[X]
+ *  $VVV,Y / $VVVV,Y  - Absolute[Y]
+ *  ($V,X) / ($VV,X)  - Indirect, X
+ *  ($V),Y / ($VV),Y  - Indirect, Y
+ *
+ *  | $[ V_Z | V_ABS ] ]
+ */
 public class CompilerTest {
     @Test
     public void firstTest(){
