19cc0e5af5db57af068d35d0c3b05a3a77e3bc16
rossdrew <thek1ng13@hotmail.com>
2017-04-27 21:08:11 +0100

    Extending Javadoc for OpCode class

--- src/main/java/com/rox/emu/p6502/op/OpCode.java
+++ src/main/java/com/rox/emu/p6502/op/OpCode.java
@@ -193,6 +193,12 @@ public enum OpCode {
         this.description = OpCodeConverter.toDescription(this.name());
     }
 
+    /**
+     * Get the OpCode for
+     *
+     * @param byteValue this byte value
+     * @return the OpCode associated with this byte value
+     */
     public static OpCode from(int byteValue){
         for (OpCode opcode : OpCode.values()){
             if (opcode.getByteValue() == byteValue)
@@ -202,6 +208,12 @@ public enum OpCode {
         throw new UnknownOpCodeException("Unknown operation code value while creating OpCode object: " + Integer.toHexString(byteValue), byteValue);
     }
 
+    /**
+     * Get the OpCode for
+     *
+     * @param opCodeName Three character {@link String} representing an {@link AddressingMode#IMPLIED} addressed OpCode
+     * @return The OpCode instance associated with this name in {@link AddressingMode#IMPLIED}
+     */
     public static OpCode from(String opCodeName){
         for (OpCode opcode : OpCode.values()){
             if (opcode.getOpCodeName().equalsIgnoreCase(opCodeName))
@@ -211,6 +223,13 @@ public enum OpCode {
         throw new UnknownOpCodeException("Unknown opcode name while creating OpCode object: " + opCodeName, opCodeName);
     }
 
+    /**
+     * Get the OpCode for
+     *
+     * @param opCodeName Three character {@link String} representing OpCode name
+     * @param addressingMode The {@link AddressingMode} of the OpCode
+     * @return The OpCode instance associated with this name in this {@link AddressingMode}
+     */
     public static OpCode from(String opCodeName, AddressingMode addressingMode){
         for (OpCode opcode : OpCode.values()){
             if (opcode.getOpCodeName().equalsIgnoreCase(opCodeName)) {
