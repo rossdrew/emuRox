51bedb1df95bbc398486da2a6ee98493964bb11d
rossdrew <thek1ng13@hotmail.com>
2017-05-21 19:19:30 +0100

    Cleaning up code

--- src/main/java/com/rox/emu/processor/mos6502/rom/InesRom.java
+++ src/main/java/com/rox/emu/processor/mos6502/rom/InesRom.java
@@ -21,21 +21,18 @@ public final class InesRom {
     }
 
     private static InesRomHeader processHeader(final int[] bytes){
-        int byteIndex = 0;
-
         if (bytes.length < InesRomHeader.HEADER_SIZE)
             throw new UnknownRomException("Invalid iNES header: Too short.");
 
-        if ((bytes[byteIndex++] != 0x4E) ||
-            (bytes[byteIndex++] != 0x45) ||
-            (bytes[byteIndex++] != 0x53) ||
-            (bytes[byteIndex++] != 0x1A)){
+        final String prefix = "" + (char)bytes[0] + (char)bytes[1] + (char)bytes[2];
+
+        if (!prefix.equals("NES") || (bytes[3] != 0x1A)){
 
             throw new UnknownRomException("Invalid iNES header: iNES prefix missing.");
         }
 
-        int prgBlocks = bytes[byteIndex++];
-        int chrBlocks = bytes[byteIndex++];
+        int prgBlocks = bytes[4];
+        int chrBlocks = bytes[5];
 
         return new InesRomHeader("NES ROM", prgBlocks, chrBlocks);
     }
