a9e42baf451806fb6dae73acb4f903058e59c965
rossdrew <thek1ng13@hotmail.com>
2017-06-09 10:16:02 +0100

    Yet more Javadoc expansion

--- src/main/java/com/rox/emu/processor/mos6502/op/AddressingMode.java
+++ src/main/java/com/rox/emu/processor/mos6502/op/AddressingMode.java
@@ -8,17 +8,47 @@ import com.rox.emu.UnknownOpCodeException;
  * @author Ross Drew
  */
 public enum AddressingMode {
+    /** Expects no argument */
     IMPLIED("Implied", 1),
+
+    /** Expects a one byte argument that is a literal value for use in the operation */
     IMMEDIATE("Immediate", 2),
+
+    /** Expects a one byte argument that contains a zero page address to use in the operation */
     ZERO_PAGE("Zero Page", 2),
+
+    /** Expects a one byte argument that contains a zero page address and the X Register to be filled with an
+     *  offset value, to use in the operation */
     ZERO_PAGE_X("Zero Page [X]", 2),
+
+    /** Expects a one byte argument that contains a zero page address and the Y Register to be filled with an
+     *  offset value, to use in the operation */
     ZERO_PAGE_Y("Zero Page [Y]", 2),
+
+    /** Expects a 2 byte argument that contains an absolute address for use in the operation*/
     ABSOLUTE("Absolute", 3),
+
+    /** Expects a 2 byte argument that contains an absolute address and the X Register to be filled with an
+     *  offset value, to use in the operation */
     ABSOLUTE_X("Absolute [X]", 3),
+
+    /** Expects a 2 byte argument that contains an absolute address and the Y Register to be filled with an
+     *  offset value, to use in the operation */
     ABSOLUTE_Y("Absolute [Y]", 3),
+
+    /** Expects a one byte argument that contains a zero page address that contains the two byte address,
+     *  to use in the operation */
     INDIRECT("Indirect", 2),
+
+    /** Expects a one byte argument that contains a zero page address and the X Register to be filled with
+     *  an offset to this address, that points to a two byte address in memory, to be used in the operation */
     INDIRECT_X("Indirect, X", 2),
+
+    /** Expects a one byte argument that contains a zero page address and the Y Register to be filled with
+     *  an offset to this address, that points to a two byte address in memory, to be used in the operation  */
     INDIRECT_Y("Indirect, Y", 2),
+
+    /** Expects no argument, operation will be performed using the Accumulator Register*/
     ACCUMULATOR("Accumulator", 1);
 
     private final String name;
@@ -29,15 +59,31 @@ public enum AddressingMode {
         this.instructionBytes = instructionBytes;
     }
 
+    /**
+     * @return the {@link String} description of this addressing mode
+     */
     public String getDescription(){
         return name;
     }
 
+    /**
+     * @return the number of bytes needed to make up this instruction, including the instruction byte
+     */
     public int getInstructionBytes(){
         return this.instructionBytes;
     }
 
-    public AddressingMode xIndexed(){
+    /**
+     * Convert this addressing mode, to x indexed. Instructions that can be X Indexed are:-
+     * <ul>
+     *     <li>{@link #ZERO_PAGE}</li>
+     *     <li>{@link #ABSOLUTE}</li>
+     *     <li>{@link #INDIRECT}</li>
+     * </ul>
+     * @return the {@link AddressingMode} that corresponds to this {@link AddressingMode}, but indexed by X
+     * @throws UnknownOpCodeException if there is an attempt to X index an addressing mode that cannot be X indexed
+     */
+    public AddressingMode xIndexed() throws UnknownOpCodeException{
         if (this == ZERO_PAGE){
             return ZERO_PAGE_X;
         }else if (this == ABSOLUTE) {
@@ -49,6 +95,16 @@ public enum AddressingMode {
         }
     }
 
+    /**
+     * Convert this addressing mode, to y indexed. Instructions that can be Y Indexed are:-
+     * <ul>
+     *     <li>{@link #ZERO_PAGE}</li>
+     *     <li>{@link #ABSOLUTE}</li>
+     *     <li>{@link #INDIRECT}</li>
+     * </ul>
+     * @return the {@link AddressingMode} that corresponds to this {@link AddressingMode}, but indexed by Y
+     * @throws UnknownOpCodeException if there is an attempt to Y index an addressing mode that cannot be Y indexed
+     */
     public AddressingMode yIndexed(){
         if (this == ZERO_PAGE){
             return ZERO_PAGE_Y;
