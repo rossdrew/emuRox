6a956f4cb5436a56f91619c28ea741c22e02120a
rossdrew <thek1ng13@hotmail.com>
2017-06-22 09:37:22 +0100

    Expanded javadoc

--- src/main/java/com/rox/emu/processor/mos6502/op/util/OpCodeConverter.java
+++ src/main/java/com/rox/emu/processor/mos6502/op/util/OpCodeConverter.java
@@ -1,23 +1,36 @@
 package com.rox.emu.processor.mos6502.op.util;
 
 import com.rox.emu.UnknownOpCodeException;
+import com.rox.emu.processor.mos6502.Mos6502;
 import com.rox.emu.processor.mos6502.op.AddressingMode;
 import com.rox.emu.processor.mos6502.op.OpCode;
 
 
 /**
- * Utility for converting internal opcode representation names to human readable descriptions
+ * Utility for converting internal {@link Mos6502} {@link OpCode} representation names to human readable descriptions
  *
- * e.g.  <code>ADC_I</code>  &rarr;  "<em>ADC (Immediate)</em>"
+ * e.g.  <code>ADC_I</code> &rarr; "<em>ADC (Immediate)</em>"
  *
  * @author Ross Drew
  */
 public class OpCodeConverter {
+    /**
+     * Extract a command line op-code of the {@link Mos6502} instruction set from the {@link String} representation of an {@link OpCode}.
+     *
+     * @param internalOpCodeName the {@link String} representation of an {@link OpCode}
+     * @return A {@link String} instruction from the {@link Mos6502} instruction set, associated with the intended {@link OpCode}
+     */
     public static String getOpCode(String internalOpCodeName){
         final String tokens[] = internalOpCodeName.split(OpCode.TOKEN_SEPARATOR);
         return tokens[OpCode.CODE_I];
     }
 
+    /**
+     * Extract the {@link Mos6502} {@link AddressingMode} from the {@link String} representation of an {@link OpCode}.
+     *
+     * @param internalOpCodeName the {@link String} representation of an {@link OpCode}
+     * @return An {@link AddressingMode} object that represents the intended addressing mode of the {@link OpCode} in question
+     */
     public static AddressingMode getAddressingMode(String internalOpCodeName){
         final String tokens[] = internalOpCodeName.split(OpCode.TOKEN_SEPARATOR);
         if (tokens.length <= OpCode.ADDR_I)
