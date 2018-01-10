301e720da3056f74f25e51a3ef47c99b9a19cae6
rossdrew <thek1ng13@hotmail.com>
2018-01-10 09:34:20 +0000

    [iNes] Data driven tests for control options byte of header

---
+++ src/test/groovy/com/rox/emu/rom/InesRomSpec.groovy
@@ -0,0 +1,41 @@
+package com.rox.emu.rom
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.rox.emu.rom.RomControlOptions.Mirroring.*
+
+
+class InesRomSpec extends Specification {
+    @Unroll
+    def "ROM Control Options Byte: #description"(){
+        given:
+        final InesRom rom = InesRom.from(asPaddedHeader([0x4E, 0x45, 0x53, 0x1A, 0x0, 0x0, controlOptionsByte] as int[]))
+
+        when:
+        final InesRomHeader header = rom.getHeader()
+        final RomControlOptions romCtrlOptions = header.getRomControlOptions()
+
+        then:
+        romCtrlOptions.ramPresent == isRamPresent
+        romCtrlOptions.trainerPresent == isTrainerPresent
+        romCtrlOptions.getMirroring() == mirroring
+
+        where:
+        controlOptionsByte || isRamPresent | isTrainerPresent | mirroring   | description
+        0b11111111         || true         | true             | FOUR_SCREEN | "All bits switched on"
+        0b11110111         || true         | true             | VERTICAL    | "Four screen set to off, VERTICAL on"
+        0b11110110         || true         | true             | HORIZONTAL  | "Four screen set to off, VERTICAL off"
+        0b11111101         || false        | true             | FOUR_SCREEN | "No ram present"
+        0b11111011         || true         | false            | FOUR_SCREEN | "No trainer present"
+        0b11111001         || false        | false            | FOUR_SCREEN | "No trainer or ram present"
+        0b00000000         || false        | false            | HORIZONTAL  | "All bits switched oFF"
+
+    }
+
+    private int[] asPaddedHeader(int[] values){
+        int[] header = new int[InesRomHeader.HEADER_SIZE];
+        System.arraycopy(values, 0, header, 0, values.length);
+        return header;
+    }
+}
