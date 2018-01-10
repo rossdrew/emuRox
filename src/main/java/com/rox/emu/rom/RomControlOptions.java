7b48ffdf3212f2c570b10efa7aef37775748ad1b
rossdrew <thek1ng13@hotmail.com>
2018-01-10 11:36:03 +0000

    [iNes] Some Javadoc concerning control options byte

--- src/main/java/com/rox/emu/rom/RomControlOptions.java
+++ src/main/java/com/rox/emu/rom/RomControlOptions.java
@@ -1,14 +1,91 @@
 package com.rox.emu.rom;
 
 /**
- 76543210
- ||||||||
- |||||||+- Mirroring: 0: horizontal (vertical arrangement) (CIRAM A10 = PPU A11)
- |||||||              1: vertical (horizontal arrangement) (CIRAM A10 = PPU A10)
- ||||||+-- 1: Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory
- |||||+--- 1: 512-byte trainer at $7000-$71FF (stored before PRG data)
- ||||+---- 1: Ignore mirroring control or above mirroring bit; instead provide four-screen VRAM
- ++++----- Lower nybble of mapper number
+ <table>
+  <tr>
+    <th colspan=9>Bit</th>
+    <th>Description</th>
+  </tr>
+
+  <tr>
+     <th>7</th>
+     <th>6</th>
+     <th>5</th>
+     <th>4</th>
+     <th>3</th>
+     <th>2</th>
+     <th>1</th>
+     <th>0</th>
+     <th></th>
+     <th> </th>
+  </tr>
+
+ <tr>
+     <td style="border-right: 2px">|</td>
+     <td style="border-right: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px"> Mirroring: 0: horizontal (vertical arrangement) (CIRAM A10 = PPU A11), 1: vertical (horizontal arrangement) (CIRAM A10 = PPU A10)</td>
+ </tr>
+
+ <tr>
+     <td style="border-right: 2px">|</td>
+     <td style="border-right: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px"> 1: Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory</td>
+ </tr>
+
+ <tr>
+     <td style="border-right: 2px">|</td>
+     <td style="border-right: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px"> 1: 512-byte trainer at $7000-$71FF (stored before PRG data)</td>
+ </tr>
+
+ <tr>
+     <td style="border-right: 2px">|</td>
+     <td style="border-right: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">|</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px"> 1: Ignore mirroring control or above mirroring bit; instead provide four-screen VRAM</td>
+ </tr>
+
+ <tr>
+     <td style="border-right: 2px">|_</td>
+     <td style="border-right: 2px">|_</td>
+     <td style="border-bottom: 2px">|_</td>
+     <td style="border-bottom: 2px">|_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px">_</td>
+     <td style="border-bottom: 2px"> Lower nybble of mapper number</td>
+ </tr>
+
+ </table>
  */
 class RomControlOptions {
     /**
