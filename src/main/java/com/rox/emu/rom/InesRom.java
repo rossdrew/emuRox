f1214e994676ab7474cc157c397dc9187827c567
rossdrew <thek1ng13@hotmail.com>
2018-01-12 14:51:36 +0000

    [iNes] prgRom and chrRom loading without tests

--- src/main/java/com/rox/emu/rom/InesRom.java
+++ src/main/java/com/rox/emu/rom/InesRom.java
@@ -6,16 +6,42 @@ package com.rox.emu.rom;
  * @author Ross Drew
  */
 public final class InesRom {
-    private final InesRomHeader header;
+    public static final int PRG_ROM_BLOCK_SIZE = 16384;
+    public static final int CHR_ROM_BLOCK_SIZE = 8192;
 
-    private InesRom(final InesRomHeader header){
+    private final InesRomHeader header;
+    private final byte[] programRom;
+    private final byte[] characterRom;
+
+    private InesRom(final InesRomHeader header,
+                    final byte[] prgRom,
+                    final byte[] chrRom){
         this.header = header;
+        this.programRom = prgRom;
+        this.characterRom = chrRom;
     }
 
     public static InesRom from(final byte[] bytes) {
         final InesRomHeader newHeader = processHeader(bytes);
 
-        return new InesRom(newHeader);
+        //TODO trainer offset will be needed
+        //16-...   ROM banks, in ascending order. If a trainer is present, its 512 bytes precede the ROM bank contents. (PRG ROM (Number of 16384 byte program ROM pages))
+        final byte[] program = extractBinaryData(bytes, newHeader.getPrgBlocks() * PRG_ROM_BLOCK_SIZE, InesRomHeader.HEADER_SIZE);
+        //TODO Need to deal with 0
+        //...-EOF  VROM banks, in ascending order.  (CHR ROM (Number of 8192 byte character ROM pages (0 indicates CHR RAM)))
+        final byte[] character = extractBinaryData(bytes, newHeader.getChrBlocks() * CHR_ROM_BLOCK_SIZE, InesRomHeader.HEADER_SIZE + program.length);
+
+        return new InesRom(newHeader, program, character);
+    }
+
+    private static byte[] extractBinaryData(final byte[] bytes, final int byteCount, final int offset) {
+        byte[] prg = new byte[byteCount];
+        for (int byteIndex=0; byteIndex<byteCount; byteIndex++ ){
+            int romByteIndex = offset + byteIndex;
+            prg[byteIndex] = bytes[romByteIndex];
+            System.out.println(prg[byteIndex]);
+        }
+        return prg;
     }
 
     private static InesRomHeader processHeader(final byte[] bytes){
@@ -42,5 +68,13 @@ public final class InesRom {
 
     public InesRomHeader getHeader(){
         return header;
+    }
+
+    public byte[] getProgramRom(){
+        return this.programRom;
+    }
+
+    public byte[] getCharacterRom(){
+        return this.characterRom;
     }
 }
