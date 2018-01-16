c7b6934f0c9244b0d187429ddc42cd9b1e42b994
rossdrew <thek1ng13@hotmail.com>
2018-01-16 09:25:37 +0000

    [mem] Adding read only memory class that I forgot

---
+++ src/main/java/com/rox/emu/mem/ReadOnlyMemory.java
@@ -0,0 +1,68 @@
+package com.rox.emu.mem;
+
+import com.rox.emu.env.RoxByte;
+
+public class ReadOnlyMemory implements Memory {
+    private final RoxByte[] memoryArray;
+
+    public ReadOnlyMemory(){
+        memoryArray = new RoxByte[0x10000];
+        reset();
+    }
+
+    public ReadOnlyMemory(int size){
+        memoryArray = new RoxByte[size];
+        reset();
+    }
+
+    public ReadOnlyMemory(final int[] contents){
+        memoryArray = new RoxByte[contents.length];
+        for (int memoryIndex = 0; memoryIndex < memoryArray.length; memoryIndex++)
+            memoryArray[memoryIndex] = RoxByte.fromLiteral(contents[memoryIndex]);
+        reset();
+    }
+
+    @Override
+    public void setByteAt(int location, int byteValue) {
+        throw new RuntimeException("Cannot write to read only memory");
+    }
+
+    @Override
+    public void setBlock(int startLocation, int[] byteValues) {
+        throw new RuntimeException("Cannot write to read only memory");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getByte(int location) {
+        return memoryArray[location].getRawValue();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getWord(int location) {
+        int word = (memoryArray[location].getRawValue() << 8 | memoryArray[location+1].getRawValue());
+        return word;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int[] getBlock(int from, int to) {
+        int[] extractedData = new int[to-from];
+        for (int i=0; i<extractedData.length; i++){
+            extractedData[i] = memoryArray[from + i].getRawValue();
+        }
+        return extractedData;
+    }
+
+    @Override
+    public void reset() {
+        //Does nothing as this is read only memory
+    }
+}
