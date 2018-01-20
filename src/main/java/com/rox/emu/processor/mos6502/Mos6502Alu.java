68b03bb3f73712da8338cd10ea724b75d4f0ce94
rossdrew <thek1ng13@hotmail.com>
2018-01-20 07:34:48 +0000

    [6502-alu] Adding a Pitest note

--- src/main/java/com/rox/emu/processor/mos6502/Mos6502Alu.java
+++ src/main/java/com/rox/emu/processor/mos6502/Mos6502Alu.java
@@ -81,7 +81,7 @@ public class Mos6502Alu {
      * @return the result of the SBC operation
      */
     public RoxByte sbc(RoxByte byteA, RoxByte byteB) {
-        registers.setFlag(Registers.N);
+        registers.setFlag(Registers.N);                 //Pitest: This does nothing to tests?!
         return adc(byteA, byteB.asOnesCompliment());
     }
 
