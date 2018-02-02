ab55db6f4371513ff00ed084d46637718d3c4a9b
rossdrew <thek1ng13@hotmail.com>
2018-02-02 14:57:45 +0000

    Cleanup imports

--- src/test/java/com/rox/emu/UnknownOpcodeExceptionTest.java
+++ src/test/java/com/rox/emu/UnknownOpcodeExceptionTest.java
@@ -3,9 +3,7 @@ package com.rox.emu;
 import com.rox.emu.processor.mos6502.op.OpCode;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class UnknownOpcodeExceptionTest {
     @Test
