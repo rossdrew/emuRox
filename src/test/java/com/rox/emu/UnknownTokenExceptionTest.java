96d5501331bc285c37f42ed8d583e438d4129c6e
rossdrew <thek1ng13@hotmail.com>
2018-03-07 14:50:44 +0000

    Case where an invalid token exception contained a cause exception wasn't covered

--- src/test/java/com/rox/emu/UnknownTokenExceptionTest.java
+++ src/test/java/com/rox/emu/UnknownTokenExceptionTest.java
@@ -2,11 +2,20 @@ package com.rox.emu;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class UnknownTokenExceptionTest {
+    @Test
+    public void testCreationWithTokenAndException(){
+        UnknownTokenException e = new UnknownTokenException("This is my reason", "UNKNOWN_TOKEN", new RuntimeException());
+
+        assertNotNull(e.getMessage());
+        assertFalse(e.getMessage().isEmpty());
+        assertNotNull(e.getToken());
+        assertNotNull(e.getCause());
+        assertEquals("UNKNOWN_TOKEN", e.getToken());
+    }
+
     @Test
     public void testCreationWithToken(){
         UnknownTokenException e = new UnknownTokenException("This is my reason", "UNKNOWN_TOKEN");
@@ -14,6 +23,7 @@ public class UnknownTokenExceptionTest {
         assertNotNull(e.getMessage());
         assertFalse(e.getMessage().isEmpty());
         assertNotNull(e.getToken());
+        assertNull(e.getCause());
         assertEquals("UNKNOWN_TOKEN", e.getToken());
     }
 }
