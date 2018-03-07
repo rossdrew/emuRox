abedda94a36e9334fcee5614a571379eaabfd588
rossdrew <thek1ng13@hotmail.com>
2018-03-07 14:41:37 +0000

    Naming was incorrect on invalid token exception test

--- src/test/java/com/rox/emu/UnknownTokenExceptionTest.java
+++ src/test/java/com/rox/emu/UnknownTokenExceptionTest.java
@@ -1,6 +1,5 @@
 package com.rox.emu;
 
-import com.rox.emu.processor.mos6502.op.OpCode;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,12 +8,12 @@ import static org.junit.Assert.assertNotNull;
 
 public class UnknownTokenExceptionTest {
     @Test
-    public void testCreationWithOpCode(){
-        UnknownOpCodeException e = new UnknownOpCodeException("This is my reason", "UNKNOWN_TOKEN");
+    public void testCreationWithToken(){
+        UnknownTokenException e = new UnknownTokenException("This is my reason", "UNKNOWN_TOKEN");
 
         assertNotNull(e.getMessage());
         assertFalse(e.getMessage().isEmpty());
-        assertNotNull(e.getOpCode());
-        assertEquals("UNKNOWN_TOKEN", e.getOpCode());
+        assertNotNull(e.getToken());
+        assertEquals("UNKNOWN_TOKEN", e.getToken());
     }
 }
