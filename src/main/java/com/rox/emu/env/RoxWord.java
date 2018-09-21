6e74324529c5bee1de0ad4f98ea82b24a522b99a
rossdrew <thek1ng13@hotmail.com>
2018-09-21 16:11:13 +0100

    Fixed error where word was masking values based on byte sizes

--- src/main/java/com/rox/emu/env/RoxWord.java
+++ src/main/java/com/rox/emu/env/RoxWord.java
@@ -11,8 +11,23 @@ public final class RoxWord {
     private final int wordValue;
 
     /** Binary digit place values */
-    private static final int[] PLACE_VALUE = {1,   2,    4,    8,    16,   32,    64,    128,
-                                              256, 512,  1024, 2048, 4096, 8192,  16384, 32768};
+    private static final int[] PLACE_VALUE = {  0b0000000000000001, //Byte 1
+                                                0b0000000000000010,
+                                                0b0000000000000100,
+                                                0b0000000000001000,
+                                                0b0000000000010000,
+                                                0b0000000000100000,
+                                                0b0000000001000000,
+                                                0b0000000010000000,
+                                                0b0000000100000000, //Byte 2
+                                                0b0000001000000000,
+                                                0b0000010000000000,
+                                                0b0000100000000000,
+                                                0b0001000000000000,
+                                                0b0010000000000000,
+                                                0b0100000000000000,
+                                                0b1000000000000000
+                                              };
 
     public static final RoxWord ZERO = new RoxWord(0);
 
@@ -54,7 +69,7 @@ public final class RoxWord {
 
     private int intFromTwosComplimented(int wordValue){
         if (isBitSet(15))
-            return -((~(wordValue-1)) & 0xFF);
+            return -((~(wordValue-1)) & 0xFFFF);
         else
             return wordValue;
     }
