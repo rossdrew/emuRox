db253987de771e3a782beb38180a92ab44bd7d82
rossdrew <thek1ng13@hotmail.com>
2026-05-03 16:18:00 +0100

    Stashing some changes\n\n  Not looked at this stuff in a while.  Some might be useful

--- src/main/java/com/rox/emu/processor/mos6502/util/UnknownProgramElementException.java
+++
@@ -1,7 +0,0 @@
-package com.rox.emu.processor.mos6502.util;
-
-public class UnknownProgramElementException extends RuntimeException {
-    public UnknownProgramElementException(final String s) {
-        super(s);
-    }
-}
