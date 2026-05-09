7baea40ea9c3eec30b93cb46ed5700b512650cac
rossdrew <thek1ng13@hotmail.com>
2026-05-09 14:56:46 +0100

    Test commit, removing some commented out code

--- src/main/java/com/rox/emu/nes/NES.java
+++ src/main/java/com/rox/emu/nes/NES.java
@@ -56,48 +56,7 @@ public class NES {
         this.mainMemory = mainMemory;
     }
 
-//    private Memory setupCpuMemoryMappings(){
-//        //$0800-$0FFF	$0800	Mirrors of $0000-$07FF
-//        final int RAM_TOP = 0x07FF;
-//        final int MIRRORED_RAM_TOP = 0x0800;
-//
-//        int[] logicalRamMirror = new int[RAM_TOP - MIRRORED_RAM_TOP];
-//        int[] physicalRam = new int[RAM_TOP];
-//        for (int ramIndex = 0; ramIndex < RAM_TOP; ramIndex++) {
-//            logicalRamMirror[ramIndex] = RAM_TOP + ramIndex;
-//            physicalRam[ramIndex] = ramIndex;
-//        }
-//
-//        //$2008-$3FFF	$1FF8	Mirrors of $2000-2007 (repeats every 8 bytes)
-//        final int PPU_REG_BOT = 0x2000;
-//        final int PPU_REG_TOP = 0x2007;
-//        final int PPU_MIRROR_TOP = 0x3FFF;
-//
-//        int ppuRegisterCount = (PPU_REG_TOP - PPU_REG_BOT);
-//        int mirrors = (PPU_MIRROR_TOP - PPU_REG_TOP) / ppuRegisterCount;
-//
-//        int[] logicalPpuRegistersMirror = new int[PPU_MIRROR_TOP - PPU_REG_TOP];
-//        int[] physicalPpuRegisters = new int[PPU_REG_TOP - PPU_REG_BOT];
-//
-//        for (int mirrorIndex = 1; mirrorIndex <= mirrors; mirrorIndex++) {
-//            for (int registerIndex = 0; registerIndex < ppuRegisterCount; registerIndex++) {
-//                logicalPpuRegistersMirror[registerIndex] = PPU_REG_TOP + registerIndex;
-//                physicalPpuRegisters[registerIndex * mirrorIndex] = PPU_REG_BOT + registerIndex;
-//            }
-//        }
-//
-//        MultiSourceMemory cpuMemory = new MultiSourceMemory().maintaining(mainMemory)
-//                                                             .withMappingTo(logicalRamMirror, physicalRam, mainMemory)
-//                                                             .withMappingTo(logicalPpuRegistersMirror, physicalPpuRegisters, mainMemory);
-//
-//        return cpuMemory;
-//    }
-
     public void reset(){
-        //Program ROM space set to
-        //0xFFFC->0x80
-        //0xFFFD->0x00
-        // 0x4000 of ROM should be memory mapped to both 0x8000 and 0xC000
         mainMemory.setBlock(RoxWord.fromLiteral(0xFFFC), RoxByte.fromIntArray(new int[] {0x80, 0x00}));
     }
 }
